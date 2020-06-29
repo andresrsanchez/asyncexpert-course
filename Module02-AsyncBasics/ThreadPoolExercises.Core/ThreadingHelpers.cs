9219d00f029e4a3ea64c320ddde19ca26002d2ec
Andrés Rodríguez Sánchez <arodriguez@plainconcepts.com>
2020-06-29 09:25:28 +0200

    Done with 'ExecuteOnThread' with strange behaviour on dotnet test

--- Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
+++ Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
@@ -12,8 +12,27 @@ namespace ThreadPoolExercises.Core
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            void DoWork()
+            {
+                try
+                {
+                    var i = 0;
+                    while (i < repeats)
+                    {
+                        token.ThrowIfCancellationRequested();
+                        action();
+                        i++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorAction?.Invoke(ex);
+                }
+            }
 
-
+            var thread = new Thread(() => DoWork());
+            thread.Start();
+            thread.Join();
         }
 
         public static void ExecuteOnThreadPool(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
