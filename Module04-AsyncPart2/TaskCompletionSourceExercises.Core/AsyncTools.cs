c260180eb26a82f94cafc1119b5c019554a7e745
Andrés Rodríguez Sánchez <arodriguez@plainconcepts.com>
2020-07-10 08:14:36 +0200

    Done with module04 TaskCompletionSource part

--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -8,7 +8,33 @@ namespace TaskCompletionSourceExercises.Core
     {
         public static Task<string> RunProgramAsync(string path, string args = "")
         {
-            return Task.FromResult(string.Empty);
+            var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var process = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo(path, args)
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                }
+            };
+
+            process.Exited += (sender, eventArgs) =>
+            {
+                var senderProcess = sender as Process;
+                if (senderProcess.ExitCode != 0)
+                {
+                    tcs.SetException(new Exception(senderProcess.StandardError.ReadToEnd()));
+                }
+                else
+                {
+                    tcs.SetResult(senderProcess.StandardOutput.ReadToEnd());
+                }
+            };
+
+            process.Start();
+
+            return tcs.Task;
         }
     }
 }
