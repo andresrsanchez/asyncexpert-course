5fab60306ce9fdc113d19a1c416d2b0308ec5d62
Andrés Rodríguez Sánchez <arodriguez@plainconcepts.com>
2020-07-11 08:35:23 +0200

    Done with module04 part 2 'TaskAwaiter'

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        public static TaskAwaiter<bool> GetAwaiter(this bool value)
+        {
+            return Task.FromResult(value).GetAwaiter();
+        }
+
+        public static TaskAwaiter<TaskAwaiter<bool>> GetAwaiter(this TaskAwaiter<bool> value)
+        {
+            return Task.FromResult(value).GetAwaiter();
+        }
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
         public void OnCompleted(Action continuation)
         {
-            throw new NotImplementedException();
+            continuation();
         }
     }
 }
